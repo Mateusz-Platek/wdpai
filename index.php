1a58e1fd789eb057d65f2d0aec96a764dde4f2b7
Mateusz-Platek <m.platek1705@gmail.com>
2023-10-30 23:38:01 +0100

    Added body to index file

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bloom</title>
+</head>
+<body>
+    <?php
+        echo "Hello there";
+    ?>
+</body>
+</html>
 
-echo "Hello there";
